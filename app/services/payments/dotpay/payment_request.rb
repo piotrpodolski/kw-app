f4e8c3a9a7d89003e6e4ab60ac9a12c283f6ea50
Dariusz Finster <darek.finster@gmail.com>
2017-05-09 11:16:02 +0200

    test

--- app/services/payments/dotpay/payment_request.rb
+++ app/services/payments/dotpay/payment_request.rb
@@ -34,10 +34,8 @@ module Payments
             Failure.new(:wrong_payment_url, message: 'Błędny link płatności, skonktaktuj się z opiekunem')
           end
         else
-          fail response
-          Failure.new(:dotpay_request_error, message: 'Błąd podczas generowania linka płatności skontaktuj sie z opiekunem')
+          Failure.new(:dotpay_request_error, message: response)
         end
-
       end
     end
   end
